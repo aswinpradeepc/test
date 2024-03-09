ce5765a5f97fc0b7dc467246b8897bde16ba903e
Aswin Pradeep <100145929+aswinpradeepc@users.noreply.github.com>
2024-03-09 16:48:41 +0530

    Update hehe.sh

--- yolov9_/hehe.sh
+++ yolov9_/hehe.sh
@@ -1,47 +1,22 @@
 #!/bin/bash
 
-# Path to the directory containing all the images
-image_path="/home/aswin/PycharmProjects/yolov9/akhil/5"
-
-# Path to the YOLOv9 script
-yolov9_script="/home/aswin/PycharmProjects/yolov9/detect.py"  # Replace with the actual path
-
 # YOLOv9 configuration
 weights="/home/aswin/PycharmProjects/yolov9/yolov9-c.pt"
 device="cpu"
 save_conf="--save-conf"
 
-# Loop through each image and run the YOLOv9 detection
-for image in "$image_path"/*; do
-    python3 "$yolov9_script" --weights "$weights" --source "$image" --device "$device" "$save_conf" --save-txt
+# List of folder names
+folder_names=("16_9" "farhan_1" "farhan_2" "s23")
+
+# Loop through each folder name and run YOLOv9 detection
+for folder_name in "${folder_names[@]}"; do
+    image_path="/home/aswin/PycharmProjects/yolov9/exp_2_lens/$folder_name"
+    yolov9_script="/home/aswin/PycharmProjects/yolov9/detect.py"  # Replace with the actual path
+
+    # Loop through each image in the current path
+    for image in "$image_path"/*; do
+        python3 "$yolov9_script" --weights "$weights" --source "$image" --device "$device" "$save_conf" --save-txt
+    done
+
+    echo "Folder $folder_name done" >> "/home/aswin/PycharmProjects/yolov9/result_exp2_lens.csv"
 done
-: <<'COMMENT'
-echo "Folder 1 done" >> "/home/aswin/PycharmProjects/yolov9/result_varsha.csv"
-
-image_path="/home/aswin/PycharmProjects/yolov9/varsha/2"
-for image in "$image_path"/*; do
-    python3 "$yolov9_script" --weights "$weights" --source "$image" --device "$device" "$save_conf" --save-txt
-done
-
-echo "Folder 2 done" >> "/home/aswin/PycharmProjects/yolov9/result_varsha.csv"
-
-image_path="/home/aswin/PycharmProjects/yolov9/varsha/3"
-for image in "$image_path"/*; do
-    python3 "$yolov9_script" --weights "$weights" --source "$image" --device "$device" "$save_conf" --save-txt
-done
-
-echo "Folder 3 done" >> "/home/aswin/PycharmProjects/yolov9/result_varsha.csv"
-
-image_path="/home/aswin/PycharmProjects/yolov9/varsha/4"
-for image in "$image_path"/*; do
-    python3 "$yolov9_script" --weights "$weights" --source "$image" --device "$device" "$save_conf" --save-txt
-done
-
-echo "Folder 4 done" >> "/home/aswin/PycharmProjects/yolov9/result_varsha.csv"
-
-image_path="/home/aswin/PycharmProjects/yolov9/varsha/5"
-for image in "$image_path"/*; do
-    python3 "$yolov9_script" --weights "$weights" --source "$image" --device "$device" "$save_conf" --save-txt
-done
-
-echo "Folder 5 done" >> "/home/aswin/PycharmProjects/yolov9/result_varsha.csv"
