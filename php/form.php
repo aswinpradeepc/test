399fd3eee01209091dc04666e206e0047dc87d73
aswinpradeepc <aswinpradeepc@gmail.com>
2026-08-08 01:04:36 +0530

    add validation to form

--- php/form.php
+++ php/form.php
@@ -1,5 +1,6 @@
 <form action="process.php" method="POST">
-	Name: <input type="text" name="name">
-	<button type="submit">submit</button>
+	name: <input type="text" name="name"><br>
+	email: <input type="email" name="email"><br>
+	password: <input type="password" name="passwd"><br>
+	<button type="submit">Submit</button>
 </form>
-<a href="http://localhost:8000">back</a>
--- php/process.php
+++ php/process.php
@@ -1,7 +1,30 @@
 <?php
 
+$errors = [];
+
 $name = trim($_POST['name'] ?? " ");
-echo htmlspecialchars("$name")
-?>
-<br>
-<a href="http://localhost:8000">back</a>
+$email = trim($_POST['email'] ?? " ");
+$pass = $_POST['passwd'];
+
+if (strlen($pass) <6){
+	$errors["pass"] = "Password should be longer than 6 chars";
+}
+
+if ($name==""){
+	$errors["name"] = "name is required";
+	}
+	
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+	$errors["email"] = "email should be valid";
+	}
+	
+if (empty($errors)){
+	echo "<p>you entered your name: ".htmlspecialchars($name)."</p>";
+	echo "<p>your email: ". htmlspecialchars($email)."</p>";
+	echo "<p>your pass: ". htmlspecialchars($pass)."</p>";
+}else{
+	foreach($errors as $e){
+	echo "$e and $errors[$e]";
+}}
+
+echo "<a href='http://localhost:8000/form.php' >back to the form</a>";
